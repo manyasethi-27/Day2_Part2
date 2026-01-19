1aa07450c21e9a4690f0aedab15f3cb7938830ce
manyasethi-27 <manya.sethi_cs23@gla.ac.in>
2026-01-19 15:23:49 +0530

    added 5 students

--- src/main/java/com/example/day2_part2/controller/Home.java
+++ src/main/java/com/example/day2_part2/controller/Home.java
@@ -5,11 +5,18 @@ import com.example.day2_part2.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 public class Home {
-    @GetMapping("/")
-    public StudentModel getStudent(){
-        StudentModel student = new StudentModel(1, "Manya", "manyasethi2710@gmail.com");
-        return student;
-    }
+        @GetMapping("/")
+        public ArrayList<StudentModel> getStudents() {
+            ArrayList<StudentModel> students = new ArrayList<>();
+            students.add(new StudentModel(1, "Manya", "manyasethi2710@gmail.com"));
+            students.add(new StudentModel(2, "Gauri", "gauri@gmail.com"));
+            students.add(new StudentModel(3, "Rahul", "rahulk@gmail.com"));
+            students.add(new StudentModel(4, "Naman", "naman27@gmail.com"));
+            students.add(new StudentModel(5, "Gun", "gun10@gmail.com"));
+            return students;
+        }
 }
